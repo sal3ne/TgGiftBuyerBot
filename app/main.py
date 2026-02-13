0c975cc15852bf12f4840edc71c03f77aca0c08a
sal3ne <bin875045@gmail.com>
2026-02-14 00:00:46 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 import asyncio
 import threading
 from app.healthcheck import run_server
+from app.infrastructure.telegram.bot import main
 
+# Запускаем healthcheck в отдельном потоке
 threading.Thread(target=run_server, daemon=True).start()
 
 if __name__ == "__main__":
